78ca2393f165ed106b7ee1bd9c652551dde46fea
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-09-19 22:02:19 -0300

    Added FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from 'faunadb'
 import NextAuth from 'next-auth'
 import Provider from 'next-auth/providers'
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -8,5 +11,26 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: 'read:user'
     })
-  ]
+  ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(q.Match(q.Index('user_by_email'), q.Casefold(email)))
+            ),
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(email)))
+          )
+        )
+        return true
+      } catch (error) {
+        console.log(user)
+        console.log('vixe', error)
+        return false
+      }
+    }
+  }
 })
